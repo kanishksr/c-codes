4c514cfd8b221b88dd95e7e8ffc4c186ecf6e887
Kanishk  S R <89303800+kanishksr@users.noreply.github.com>
2022-08-21 22:36:16 +0530

    Create LCM of two numbers.c

---
+++ LCM of two numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int i,num1,num2,gcd,lcm;
+    printf("Enter 2 numbers:");
+    scanf("%d %d",&num1,&num2);
+    
+    for(i=1;i<=num1 && i<=num2;i++)
+    {
+        if (num1%i==0 && num2%i==0){
+       gcd=i;
+        }    
+    }
+    lcm=(num1*num2)/gcd;
+    printf("LCM is %d",lcm);
+}
